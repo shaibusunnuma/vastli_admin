7abb16d13b0f30db33eedd88003803f38f7a3122
Shaibu Sunnuma <shaibusunnuma@gmail.com>
2025-04-15 20:58:30 +0000

    Refactor user API slice to operate on operators, updating endpoints and types accordingly.

--- lib/services/users/userApiSlice.ts
+++ lib/services/users/userApiSlice.ts
@@ -1,55 +1,74 @@
 import { createApi } from "@reduxjs/toolkit/query/react";
 import { axiosBaseQuery } from "@/lib/baseQuery";
 import { providesList } from "../../common";
-import { AuthUser } from "@/types/auth.types";
+import { Operator } from "@/types/users";
 
 export const userApiSlice = createApi({
   reducerPath: "userApi",
   baseQuery: axiosBaseQuery({ baseUrl: "operators" }),
-  tagTypes: ["Users"],
+  tagTypes: ["Operator"],
   endpoints: (build) => ({
-    getUsers: build.query<AuthUser[], Partial<AuthUser>>({
+    getOperators: build.query<Operator[], Partial<Operator>>({
       query: (filter) => ({
         url: "",
         params: { filter: { ...filter, page: 1 } },
       }),
-      providesTags: (result, error) => providesList(result, "Users"),
+      providesTags: (result, error) => providesList(result, "Operator"),
     }),
 
-    getUserById: build.query<AuthUser, string>({
+    getOperatorById: build.query<Operator, string>({
       query: (userId) => ({
         url: `/${userId}`,
       }),
-      providesTags: (result, error, userId) => [{ type: "Users", id: userId }],
+      providesTags: (result, error, userId) => [{ type: "Operator", id: userId }],
     }),
 
-    inviteUser: build.mutation<AuthUser, Partial<AuthUser> & { restaurant?: string }>({
+    inviteOperator: build.mutation<Operator, Partial<Operator> & { restaurant?: string }>({
       query: ({ restaurant, ...data }) => ({
         url: "/invite",
         method: "POST",
         data,
         params: { restaurant },
       }),
-      invalidatesTags: [{ type: "Users", id: "LIST" }],
+      invalidatesTags: [{ type: "Operator", id: "LIST" }],
     }),
 
-    updateUser: build.mutation<AuthUser, Partial<AuthUser>>({
-      query: (updatedUser) => ({
-        url: `/${updatedUser.id}`,
-        method: "PATCH",
-        data: updatedUser,
+    addOperator: build.mutation<Operator, Partial<Operator>>({
+      query: (data) => ({
+        url: "",
+        method: "POST",
+        data,
       }),
-      invalidatesTags: (result, error, { id }) => [{ type: "Users", id }],
+      invalidatesTags: (result, error, { id }) => [
+        { type: "Operator", id },
+        { type: "Operator", id: "LIST" },
+      ],
     }),
 
-    deleteUser: build.mutation<void, string>({
+    updateOperator: build.mutation<Operator, Partial<Operator>>({
+      query: (updatedOperator) => ({
+        url: `/${updatedOperator.id}`,
+        method: "PATCH",
+        data: updatedOperator,
+      }),
+      invalidatesTags: (result, error, { id }) => [{ type: "Operator", id }],
+    }),
+
+    deleteOperator: build.mutation<void, string>({
       query: (userId) => ({
         url: `/${userId}`,
         method: "DELETE",
       }),
-      invalidatesTags: (result, error, userId) => [{ type: "Users", id: userId }],
+      invalidatesTags: (result, error, userId) => [{ type: "Operator", id: userId }],
     }),
   }),
 });
 
-export const { useGetUsersQuery, useGetUserByIdQuery, useInviteUserMutation, useUpdateUserMutation, useDeleteUserMutation } = userApiSlice;
+export const {
+  useGetOperatorsQuery,
+  useGetOperatorByIdQuery,
+  useInviteOperatorMutation,
+  useUpdateOperatorMutation,
+  useDeleteOperatorMutation,
+  useAddOperatorMutation,
+} = userApiSlice;
