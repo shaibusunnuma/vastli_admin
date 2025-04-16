9cbaae36f7e56f55df33d5661c6b73b2ecb54e92
Shaibu Sunnuma <shaibusunnuma@gmail.com>
2025-04-16 12:12:49 +0000

    add stats to slice

--- lib/features/restaurants/restaurantSlice.ts
+++ lib/features/restaurants/restaurantSlice.ts
@@ -1,11 +1,19 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { Restaurant } from "@/types/restaurants";
+import { CustomerStats } from "@/types/users";
+import { ReservationStats } from "@/types/reservations";
+
+interface RestaurantStats {
+  customerStats?: CustomerStats;
+  reservationStats?: ReservationStats;
+}
 
 interface RestaurantState {
   restaurants: Restaurant[];
   activeRestaurant: Restaurant | null;
   isLoading: boolean;
   error: string | null;
+  stats: RestaurantStats | null;
 }
 
 const initialState: RestaurantState = {
@@ -13,8 +21,8 @@ const initialState: RestaurantState = {
   activeRestaurant: null,
   isLoading: false,
   error: null,
+  stats: null,
 };
-
 
 export const restaurantSlice = createSlice({
   name: "restaurant",
@@ -30,17 +38,20 @@ export const restaurantSlice = createSlice({
         state.activeRestaurant = payload[0];
       }
     },
+    setRestaurantStats: (state, action: PayloadAction<RestaurantStats>) => {
+      state.stats = action.payload;
+    },
   },
   selectors: {
     selectRestaurants: (res) => res.restaurants,
     selectRestaurant: (res) => res.activeRestaurant,
     selectIsLoading: (res) => res.isLoading,
     selectError: (res) => res.error,
+    selectRestaurantStats: (res) => res.stats,
   },
 });
 
-export const { setRestaurant, setRestaurants } = restaurantSlice.actions;
-export const { selectRestaurant, selectRestaurants, selectIsLoading, selectError } =
-  restaurantSlice.selectors;
+export const { setRestaurant, setRestaurants, setRestaurantStats } = restaurantSlice.actions;
+export const { selectRestaurant, selectRestaurants, selectIsLoading, selectError, selectRestaurantStats } = restaurantSlice.selectors;
 
 export default restaurantSlice.reducer;
