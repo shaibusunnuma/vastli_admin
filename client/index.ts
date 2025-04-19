f351bd4ead1e8d015155cf35d5fceda09a56765b
Shaibu Sunnuma <shaibusunnuma@gmail.com>
2025-04-19 19:18:11 +0000

    handle refresh token interceptor

--- client/index.ts
+++ client/index.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { SERVER_URL } from "@/constants";
+import { authClient } from "@/lib/auth/auth-client";
+import { getDeviceInfo } from "@/lib/device-info";
 
 const axiosClient = axios.create({
   baseURL: SERVER_URL,
@@ -9,5 +11,26 @@ const axiosClient = axios.create({
   },
   withCredentials: true, // This is crucial for sending cookies with requests
 });
+
+axiosClient.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    if (error.response.status === 401) {
+      try {
+        await authClient.refreshToken();
+        const originalRequest = error.config;
+        const deviceInfo = getDeviceInfo();
+        originalRequest.headers["x-device-id"] = deviceInfo.deviceId;
+
+        return axiosClient(originalRequest);
+      } catch (refreshError) {
+        return Promise.reject(refreshError);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default axiosClient;
--- lib/auth/auth-client.ts
+++ lib/auth/auth-client.ts
@@ -1,5 +1,5 @@
 import { SignInParams } from "@/types/auth.types";
-import axios, { AxiosError, AxiosRequestConfig } from "axios";
+import axios from "axios";
 import { SERVER_URL } from "@/constants";
 import ApiErrorHandler from "../error-handler";
 import logger from "@/lib/logger";
@@ -14,59 +14,6 @@ const client = axios.create({
   },
   withCredentials: true,
 });
-
-let isRefreshing = false;
-let failedQueue: { resolve: (value: unknown) => void; reject: (reason?: unknown) => void }[] = [];
-
-const processQueue = (error: unknown | null, token: string | null = null) => {
-  failedQueue.forEach(promise => {
-    if (error) {
-      promise.reject(error);
-    } else {
-      promise.resolve(token);
-    }
-  });
-  
-  failedQueue = [];
-};
-
-client.interceptors.response.use(
-  (response) => response,
-  async (error: AxiosError) => {
-    const originalRequest = error.config as AxiosRequestConfig & { _retry?: boolean };
-    
-    // If error is not 401 or request has already been retried, reject
-    if (!error.response || error.response.status !== 401 || originalRequest._retry) {
-      return Promise.reject(error);
-    }
-    
-    // If we're already refreshing, queue this request
-    if (isRefreshing) {
-      return new Promise((resolve, reject) => {
-        failedQueue.push({ resolve, reject });
-      }).then(() => {
-        return client(originalRequest);
-      }).catch(err => {
-        return Promise.reject(err);
-      });
-    }
-    
-    originalRequest._retry = true;
-    isRefreshing = true;
-    
-    try {
-      const { data } = await client.post('refresh-token');
-      isRefreshing = false;
-      processQueue(null, data.access_token);
-      return client(originalRequest);
-    } catch (refreshError) {
-      isRefreshing = false;
-      processQueue(refreshError);
-      logger.error('Token refresh failed', refreshError);
-      return Promise.reject(refreshError);
-    }
-  }
-);
 
 export const authClient = {
   async signIn(params: Omit<SignInParams, 'deviceInfo'>) {
