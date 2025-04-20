39f3ed04fa59e4f27cb90002c2963033be86c72a
Shaibu Sunnuma <shaibusunnuma@gmail.com>
2025-04-20 13:42:45 +0000

    date range picker

---
+++ views/analytics/DateRangePicker.tsx
@@ -0,0 +1,26 @@
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+
+interface DateRangePickerProps {
+  onSelectRange: (range: string) => void;
+  selectedRange: string;
+}
+
+export const DateRangePicker: React.FC<DateRangePickerProps> = ({ onSelectRange, selectedRange }) => {
+  return (
+    <div className="grid gap-2">
+      <Select onValueChange={onSelectRange} value={selectedRange}>
+        <SelectTrigger id="date-range" className="[&>span]:text-ellipsis [&>span]:overflow-hidden [&>span]:w-full">
+          <SelectValue placeholder="Select date range" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="today">Today</SelectItem>
+          <SelectItem value="last7d">Last 7 Days</SelectItem>
+          <SelectItem value="last30d">Last 30 Days</SelectItem>
+          <SelectItem value="thisMonth">This Month</SelectItem>
+          <SelectItem value="lastMonth">Last Month</SelectItem>
+          <SelectItem value="thisYear">This Year</SelectItem>
+        </SelectContent>
+      </Select>
+    </div>
+  );
+};
